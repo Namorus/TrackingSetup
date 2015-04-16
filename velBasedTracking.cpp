55bccfaafcfd0f948d04f0dfcc57e7bb61cec79c
mantelt <thomas.mantel@mavt.ethz.ch>
2015-04-16 18:08:48 +0200

    fixing error in calculation of angular rates

--- velBasedTracking.cpp
+++ velBasedTracking.cpp
@@ -12,7 +12,7 @@ namespace tracking {
 
 VelBasedGpsTrackingMode::VelBasedGpsTrackingMode(TrackingEstimator* estimator):
 		estimator_(estimator), panOffset_(0), tiltOffset_(0), magneticDeclination_(0), hardPositioning_(false),
-	azimuthAngle_(0), elevationAngle_(0), azimuthRate(0), elevationRate_(0) {
+	azimuthAngle_(0), elevationAngle_(0), azimuthRate_(0), elevationRate_(0) {
 	// TODO Auto-generated constructor stub
 
 }
@@ -58,9 +58,9 @@ void VelBasedGpsTrackingMode::update(double curPanAngle, double curTiltAngle) {
 	}
 
 	// 1. Calculate pan and tilt speeds from pos & vel
-	azimuthRate = -(targetLocalPos_.x*targetLocalVel_.y-targetLocalPos_.y*targetLocalVel_.x)/(targetLocalPos_.x*targetLocalPos_.x+targetLocalPos_.y*targetLocalPos_.y);
+	azimuthRate_ = -(targetLocalPos_.x*targetLocalVel_.y-targetLocalPos_.y*targetLocalVel_.x)/(targetLocalPos_.x*targetLocalPos_.x+targetLocalPos_.y*targetLocalPos_.y);
 	double elevationRateNumerator = ((targetLocalPos_.x*targetLocalPos_.x+targetLocalPos_.y*targetLocalPos_.y)*targetLocalVel_.z-targetLocalPos_.z*(targetLocalPos_.x*targetLocalVel_.x+targetLocalPos_.y*targetLocalVel_.y));
-	double elevationRateDenominator = (sqrt(targetLocalPos_.x*targetLocalPos_.x+targetLocalPos_.y*targetLocalPos_.y)*sqrt(targetLocalPos_.x*targetLocalPos_.x+targetLocalPos_.y*targetLocalPos_.y+targetLocalPos_.z*targetLocalPos_.z));
+	double elevationRateDenominator = (sqrt(targetLocalPos_.x*targetLocalPos_.x+targetLocalPos_.y*targetLocalPos_.y)*(targetLocalPos_.x*targetLocalPos_.x+targetLocalPos_.y*targetLocalPos_.y+targetLocalPos_.z*targetLocalPos_.z));
 	if (elevationRateDenominator > 0) {
 		elevationRate_ = elevationRateNumerator / elevationRateDenominator;
 	} else {
@@ -69,11 +69,11 @@ void VelBasedGpsTrackingMode::update(double curPanAngle, double curTiltAngle) {
 
 
 	// 2. correction of speed based on measured angle
-	double newPanSpeed = estimator_->rad2deg(azimuthRate) - 0.5*(azimuthAngle_ - curPanAngle); // TODO: make gain configurable
+	double newPanSpeed = estimator_->rad2deg(azimuthRate_) - 0.5*(azimuthAngle_ - curPanAngle); // TODO: make gain configurable
 	double newTiltSpeed = estimator_->rad2deg(elevationRate_) - 0.5*(elevationAngle_ - curTiltAngle); // TODO: make gain configurable
 
 	// 3. Update setpoint
-
+	logmessage.str("Velocity based GPS tracking | ");
 	if (isnan(newPanSpeed)) {
 		logmessage << "New pan speed is nan -> stopping";
 		setNewSetpoints(ct_velocity, 0, 0);
